c898e141c8a1a4337837da6806b04f79ca42f295
Adrian Ferrari <sdadrian@gmail.com>
2022-12-29 02:34:00 -0300

    added loading <p>

--- client/src/components/SingleProduct.js
+++ client/src/components/SingleProduct.js
@@ -1,48 +1,59 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useNavigate, useParams } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
 import { add } from "../redux-store/cartSlice";
 import "../styles/products.css";
 
 function SingleProduct() {
+    const [data, setData] = useState()
+    const [isLoading, setIsLoading] = useState(true)
     const navigate = useNavigate();
     const paramId = parseInt(useParams().productId);
     const dispatch = useDispatch();
-    const product = useSelector((state) => state.products[paramId]);
-    const { imgName, title, cost, description } = product;
+    const product = useSelector((state) => state?.products[paramId]);
     
     function addToCart() {
-        dispatch(add(product));
+        dispatch(add(data));
+    }
+
+    function getData() {
+        setData({...product})
+        setIsLoading(false)
     }
 
     useEffect(() => {
+        getData()
         window.scrollTo(0, 0);
-    }, []);
+    }, [product]);
 
     return (
         <div className="single-page-container">
+            {isLoading ? <p>Loading...</p> :
+            <>
             <button className="go-back-arrow" onClick={() => navigate(-1)}>
                 &#11013; Go back
             </button>
             <div className="single-product">
                 <div className="single-product-info">
-                    <img src={`../images/${imgName}.jpg`} alt="cake" />
+                    <img src={`../images/${data.imgName}.jpg`} alt="cake" />
                     <div className={`info-product-list`}>
-                        <h3>{title}</h3>
-                        <p className="cost">${cost}</p>
-                        <p className="description">{description}</p>
+                        <h3>{data.title}</h3>
+                        <p className="cost">${data.cost}</p>
+                        <p className="description">{data.description}</p>
                         <button
                             className="single-product-button-2"
                             onClick={addToCart}
                         >
-                            Add to cart
+                            Agregar al carrito
                         </button>
                     </div>
                 </div>
                 <button className="single-product-button" onClick={addToCart}>
-                    Add to cart
+                    Agregar al carrito
                 </button>
             </div>
+            </>
+            }
         </div>
     );
 }
